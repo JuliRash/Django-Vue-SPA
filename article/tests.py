a2e04877b987b66ec4e2476e05ac34d13883ad81
julius idowu i.o <julipels@yahoo.com>
2020-10-08 21:05:26 +0100

    add simple test

--- article/tests.py
+++ article/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Article
+
+
+class ArticleModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        new_article = Article.objects.create(
+            article_title='first article', article_body='first article body')
+
+    def test_article_title(self):
+        article = Article.objects.get(id=1)
+        expected_object_name = f'{article.article_title}'
+        self.assertEquals(expected_object_name, 'first article')
+
+    def test_article_body(self):
+        article = Article.objects.get(id=1)
+        expected_object_name = f'{article.article_body}'
+        self.assertEquals(expected_object_name, 'first article body')
