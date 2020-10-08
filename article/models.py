8b575101a9cbcad53499d3fddc9b93b361be0f66
julius idowu i.o <julipels@yahoo.com>
2020-10-08 21:12:50 +0100

    update model: remove article_id field

--- article/models.py
+++ article/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
 class Article(models.Model):
-    article_id = models.AutoField(primary_key=True)
     article_title = models.CharField(max_length=300)
     article_body = models.TextField()
--- article/static/assets/main.js
+++ article/static/assets/main.js
@@ -9,68 +9,68 @@ var startapp = new Vue({
         loading: false,
         currentArticle: {},
         message: null,
-        newArticle: {'article_title': null, 'article_body': null}
-        },
-    mounted(){
+        newArticle: { 'article_title': null, 'article_body': null }
+    },
+    mounted() {
         this.getArticles();
     },
 
     methods: {
-        getArticles: function() {
+        getArticles: function () {
             this.loading = true;
             axios.get(url).then((response) => {
                 this.articles = response.data;
                 this.loading = false;
-            }).catch((err)=>{
+            }).catch((err) => {
                 this.loading = false;
                 console.log(err)
             })
-            }, 
-        getArticle: function(id){
+        },
+        getArticle: function (id) {
             // get a single article.
             this.loading = true;
-            axios.get(url + id).then((response)=> {
+            axios.get(url + id).then((response) => {
                 this.currentArticle = response.data;
                 $("#editArticleModal").modal('show');
                 this.loading = false;
-            }).catch((err)=>{
+            }).catch((err) => {
                 this.loading = false;
                 console.log(err)
             })
         },
-        addArticle: function() {
+        addArticle: function () {
             // add a new article.
             this.loading = true;
-            axios.post(url, this.newArticle).then((response)=> {
+            axios.post(url, this.newArticle).then((response) => {
                 this.loading = false;
                 this.getArticles();
-            }).catch((err)=>{
+            }).catch((err) => {
                 this.loading = false;
                 console.log(err)
             })
         },
-        updateArticle: function() {
+        updateArticle: function () {
             this.loading = true;
-            axios.put(url + this.currentArticle.article_id + '/', this.currentArticle ).then((response) =>{
+            axios.put(url + this.currentArticle.id + '/', this.currentArticle).then((response) => {
                 this.loading = false;
                 this.currentArticle = response.data;
                 this.getArticles();
-            }).catch((err)=>{
+            }).catch((err) => {
                 this.loading = false;
                 console.log(err)
             })
         },
-        deleteArticle: function(id){
+        deleteArticle: function (id) {
             this.loading = true;
-            axios.delete(url + id).then((response)=>{
+            axios.delete(url + id).then((response) => {
                 this.loading = false;
                 this.getArticles()
-            }).catch((err)=>{
+            }).catch((err) => {
                 this.loading = false;
                 console.log(err)
             })
         }
-        },
-    
-    
+    },
+
+
 });
