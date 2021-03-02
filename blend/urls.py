2e2c7340d85ca03853d1e9fface7baaf472fba8e
julius idowu i.o <julipels@yahoo.com>
2021-03-02 15:59:03 +0100

    update settings.py with swagger ui

--- blend/urls.py
+++ blend/urls.py
@@ -2,10 +2,15 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from rest_framework_swagger.views import get_swagger_view
+
 from .routers import router
+
+schema_view = get_swagger_view(title='Blog API')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-    path('', include('article.urls'))
+    path('', include('article.urls')),
+    path('swagger-docs/', schema_view),
 ]
